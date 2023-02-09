6795b050b36a05becc92da93fcc4f88300bd130c
Brian <wamwangimathenge@gmail.com>
2023-02-09 18:31:27 +0300

    Completed assignment

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,20 @@
 ## code your solution here. 
+
+
+
+class Cat
+
+    attr_accessor :name
+
+    # def initialize(name)
+    #     @name = name
+    # end
+
+    def meow
+        puts "meow!"
+    end
+end
+
+
+rose = Cat.new
+
